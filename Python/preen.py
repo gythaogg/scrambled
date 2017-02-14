13bb975d0b59a1cd31b202a44741efeac9f43571
Gytha Ogg <gythaoggscat@gmail.com>
2017-02-14 13:19:53 +0100

    removed duplicates and words with special characters, uses words at least 6 letters long

--- Python/preen.py
+++ Python/preen.py
@@ -2,7 +2,7 @@
 ''' preens the data files and filters word lists'''
 import logging
 import sys
-
+import string
 __author__ = "Gytha Ogg"
 __copyright__ = "Gytha Ogg"
 __credits__ = ["Gytha Ogg"]
@@ -12,7 +12,7 @@ __maintainer__ = "Gytha Ogg"
 __email__ = "gythaoggscat@gmail.com"
 __status__ = "development"
 
-WORD_LEN_LIMIT = 7
+WORD_LEN_LIMIT = 6
 FILE_NAME = 'data/5kWords.txt'
 
 
@@ -27,14 +27,24 @@ def preen(filename=FILE_NAME):
     with open(filename, 'r') as input_file:
         lines = input_file.readlines()
         logging.info('Read %d lines', len(lines))
-        filtered = [l[:-1] for l in lines if WORD_LEN_LIMIT < len(l)]
-        logging.info('%d lines after filtering', len(filtered))
-        outfile_name = filename + 'MIN_'+str(WORD_LEN_LIMIT)+'.out'
+        # discard short words (shorter than WORD_LEN_LIMIT)
+        # save words in lowercase
+        filtered = [l[:-1].lower()
+                    for l in lines if WORD_LEN_LIMIT < len(l)]
+        logging.info('%d lines after removing short words', len(filtered))
+        # remove duplicates
+        filtered = list(set(filtered))
+        logging.info('%d lines after removing duplicates', len(filtered))
+        outfile_name = filename + '_MIN_'+str(WORD_LEN_LIMIT)+'.out'
         logging.info('Writing to out file ... %s', outfile_name)
+
     with open(outfile_name, 'w') as outfile:
         for item in filtered:
-            outfile.write("%s\n" % item)
-            logging.info('Written %s to %s.', item, outfile_name)
+            if any(letter not in string.ascii_letters for letter in item):
+                logging.info('REJECTED %s.', item)
+            else:
+                outfile.write("%s\n" % item)
+                logging.debug('Written %s to %s.', item, outfile_name)
     return
 
 
@@ -51,7 +61,7 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s %(levelname)s %(message)s')
 
     main(sys.argv[1:])
